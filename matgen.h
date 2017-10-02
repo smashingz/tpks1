65d3f0183e2fac2b982fa0f11cc9289d616aeee2
Nemo <1110101@gmail.com>
2017-10-02 04:28:30 +0300

    Забыл добавить matgen.h

---
+++ matgen.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int create_inc_matr(char *file, int x, int y);
+
+int create_inc_matr(char *file, int x, int y) {
+	int i,j,one_pos,two_pos;
+	int *buff;
+	FILE *in=fopen(file, "w+");
+	buff=calloc(x,sizeof(int));
+	for(i=0;i<x;i++)
+		buff[i]=0;
+	fprintf(in, "%d %d\n",y,x);
+	for(i=0;i<y;i++) {
+		one_pos=rand()%10;
+		while((two_pos=rand()%10)==one_pos)
+			two_pos=rand()%10;
+		buff[one_pos]=1;
+		buff[two_pos]=2;
+		for(j=0;j<x;j++)
+			fprintf(in, "%d", buff[j]);
+		fprintf(in, "\n");
+		buff[one_pos]=buff[two_pos]=0;
+	}
+	fclose(in);
+	free(buff);
+	return 0;
+}
